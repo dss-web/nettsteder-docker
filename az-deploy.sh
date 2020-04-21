f2de6e0503af3a4377a4a0dcdcc1fb86039b4e3a
Per Søderlind <per@soderlind.no>
2020-04-21 11:09:05 +0200

    update

--- az-deploy.sh
+++ az-deploy.sh
@@ -3,12 +3,14 @@
 set -e  # stops your script if any simple command fails.
 
 LOCATION="norwayeast" #List of available regions is 'centralus,eastasia,southeastasia,eastus,eastus2,westus,westus2,northcentralus,southcentralus,westcentralus,northeurope,westeurope,japaneast,japanwest,brazilsouth,australiasoutheast,australiaeast,westindia,southindia,centralindia,canadacentral,canadaeast,uksouth,ukwest,koreacentral,koreasouth,francecentral,southafricanorth,uaenorth,australiacentral,switzerlandnorth,germanywestcentral,norwayeast'.
-RESOURCEGROUP=""
-SERVICEPLAN=""
-APPNAME=""
-ADMINPASSWORD=""
-MYSQLSERVERNAME=""
+RESOURCEGROUP="minWPRG_001"
+SERVICEPLAN="minWPRGsp_001"
+APPNAME="minMultiSite"
+ADMINPASSWORD="Hakkedun0me"
+MYSQLSERVERNAME="minMultiSite"
 MYSQLSKU="B_Gen5_2"
+STORAGE="StorageV2"
+STORAGESKU="Standard_LRS"
 
 if [ -z "$LOCATION" ] || [ -z "$RESOURCEGROUP" ] || [ -z "$SERVICEPLAN" ] || [ -z "$APPNAME" ]|| [ -z "$ADMINPASSWORD" ] || [ -z "$MYSQLSERVERNAME" ] || [ -z "$MYSQLSKU" ]; then
   echo 'one or more variables are undefined'
@@ -29,6 +31,36 @@ az mysql server create --resource-group $RESOURCEGROUP --name $MYSQLSERVERNAME  
 az mysql server firewall-rule create --name allAzureIPs --server $MYSQLSERVERNAME --resource-group $RESOURCEGROUP --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
 
 az mysql db create --resource-group $RESOURCEGROUP --server-name $MYSQLSERVERNAME --name wordpress
+
+az storage account create --name "${APPNAME}_storage" --resource-group $RESOURCEGROUP --location "$LOCATION" --sku $STORAGESKU --kind $STORAGE
+
+export storageAccountName="mystorageacct$RANDOM"
+
+az storage account create \
+    --resource-group $RESOURCEGROUP \
+    --name "${APPNAME}_storage" \
+    --location "$LOCATION" \
+    --kind $STORAGE \
+    --sku $STORAGESKU \
+    --enable-large-file-share \
+    --output none
+
+export storageAccountKey=$(az storage account keys list \
+    --resource-group $RESOURCEGROUP \
+    --account-name "${APPNAME}_storage" \
+    --query "[0].value" | tr -d '"')
+
+shareName="myshare"
+
+az storage share create \
+    --account-name "${APPNAME}_storage" \
+    --account-key $storageAccountKey \
+    --name $shareName \
+    --quota 1024 \
+    --output none
+
+
+az webapp config storage-account add --resource-group $RESOURCEGROUP --name "${APPNAME}_webstorage" --custom-id <custom_id> --storage-type AzureFiles --share-name "${APPNAME}_wordpress_file" --account-name "${APPNAME}_storage" --access-key "<access_key>" --mount-path <mount_path_directory>
 
 ## Add Settings
 
